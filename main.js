2472868c7692af340dedb9d9e108297bb9bf231e
leona <leonardo.casar8@gmail.com>
2022-02-25 11:33:04 +0100

    update

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
 function calcola(surname,name,sex,birthplace,provincia,birthday){
-    check(name);
+    console.log(check(name))
     //First 6 letters
 
 }
-function check(value){
-    const name=[];
-    let vocali=0;
-    let consonanti=0;
-    for(let i=0;name.length<3;i++){
-        if(value.charAt(i)==("A"||"E"||"I"||"O"||"U")){
-            vocali.push(value.charAt(i));
+function check(cognome,nome){
+    let consonanti=[];
+    let vocali=[];
+   for(let i=0;i<cognome.length;i++){
+       if(cognome.charAt(i)=="A"||
+           cognome.charAt(i)=="E"||
+           cognome.charAt(i)=="I"||
+           cognome.charAt(i)=="O"||
+           cognome.charAt(i)=="U"){
+           vocali.push(cognome.charAt(i));
+       }
+       else{
+            consonanti.push(cognome.charAt(i));
+       }
+   }
+   if(consonanti.length>=3){
+       cognome = consonanti[0]+consonanti[1]+consonanti[2];
+   }
+    consonanti=[];
+    vocali=[];
+    for(let i=0;i<nome.length;i++){
+        if(nome.charAt(i)=="A"||
+            nome.charAt(i)=="E"||
+            nome.charAt(i)=="I"||
+            nome.charAt(i)=="O"||
+            nome.charAt(i)=="U"){
+            vocali.push(nome.charAt(i));
         }
-        else if(value.charAt(i)!=("A"||"E"||"I"||"O"||"U")){
-            consonanti.push(value.charAt(i));
+        else{
+            consonanti.push(nome.charAt(i));
         }
     }
-    if(consonanti.length>=3){
-        return(consonanti[0]+consonanti[1]+consonanti[2]);
+    if(consonanti.length>=4){
+        cognome = consonanti[0]+consonanti[2]+consonanti[4];
     }
-    else if(vocali.length>=3){
-        result="";
-        for(let i=0;)
+    else if(consonanti.length=3){
+        cognome = consonanti[0]+consonanti[1]+consonanti[2];
+    }
+    else if(consonanti.length<3&&consonanti.length>0){
+
 
     }
-    else{
 
-    }
+
 
 }
